0bbd7ea3de8a1f4ef500d36b483e6ec1b137eb8b
Sokos6 <will.sokolowski@gmail.com>
2019-08-03 17:07:38 -0700

    finish O

--- bigO.js
+++ bigO.js
@@ -100,3 +100,74 @@ function compressBoxesTwice(boxes, boxes2) {
   });
 }
 // O(a + b)
+
+// Log all pairs of array
+const boxes = ['a', 'b', 'c', 'd', 'e'];
+
+function logAllPairsOfArray(array) {
+  for (let i =0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j])
+    }
+  }
+}
+
+logAllPairsOfArray(boxes);
+
+// nested loops are multiplication
+// O(n^2) - Quadratic time - horrible, very slow
+
+function printAllNumbersThenAllPairSums(numbers) {
+
+  console.log('these are the numbers:');
+  numbers.forEach(function(number) {
+    console.log(number);
+  });
+
+  console.log('and these are their sums:');
+  numbers.forEach(function(firstNumber) {
+    numbers.forEach(function(secondNumber) {
+      console.log(firstNumber + secondNumber);
+    });
+  });
+}
+
+printAllNumbersThenAllPairSums([1,2,3,4,5]);
+
+// O(n + n^2)
+// drop n, we care about the most important term
+// O(n^2)
+
+// #Big O Cheat Sheet:
+// -Big OsO(1) Constant- no loops
+// O(log N) Logarithmic- usually searching algorithms have log n if they are sorted (Binary Search)
+// O(n) Linear- for loops, while loops through n items
+// O(n log(n)) Log Liniear- usually sorting operations
+// O(n^2) Quadratic- every element in a collection needs to be compared to ever other element. Two
+// nested loops
+// O(2^n) Exponential- recursive algorithms that solves a problem of size N
+// O(n!) Factorial- you are adding a loop for every element
+// Iterating through half a collection is still O(n)
+// Two separate collections: O(a * b)
+// -What can cause time in a function?-
+// Operations (+, -, *, /)
+// Comparisons (<, >, ==)
+// Looping (for, while)
+// Outside Function call (function())
+// -Rule BookRule 1: Always worst Case
+// Rule 2: Remove Constants
+// Rule 3: Different inputs should have different variables. O(a+b). A and B arrays nested would be
+// O(a*b)
+// + for steps in order
+// * for nested steps
+// Rule 4: Drop Non-dominant terms
+// -What causes Space complexity?-
+// Variables
+// Data Structures
+// Function Call
+// Allocations
+
+// GOOD CODE -
+// READABLE
+// MEMORY - Space Complexity
+// SPEED - Time Complexity
